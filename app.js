d9f5ba21896a6044688c642b211a6b7cc378baa9
Lee-Joeng-Bin <jungbiiin@naver.com>
2019-07-10 12:10:58 +0900

    login design

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', minIndexRouter);
 
+app.use('/process', express.static(__dirname + '/public')); 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
