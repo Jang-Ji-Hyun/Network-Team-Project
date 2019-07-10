783bb1b95e9aaae034ab4dde727d0a9774fef0b4
Jang-Ji Hyun <s18018@gsm.hs.kr>
2019-07-10 16:20:50 +0900

    mongoClient Create and Change login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017';
-var dbName = 'NoteBookLent';
- 
+var mongoClient = require('../models/mongoClient');
+
+mongoClient.connect((err,db,client)=>{
+  client.close();
+})
 
 router.get('/', (req, res, next)=>{
   res.render("index");
@@ -12,10 +13,8 @@ router.get('/', (req, res, next)=>{
 router.get('/login', (req, res, next)=>{
   var Id = req.query.UserId;
   var Password = req.query.UserPassword;
-
-  MongoClient.connect(url,{useNewUrlParser:true}, function(err, client){
-    const db = client.db(dbName);
-
+  
+  mongoClient.connect((err,db,client)=>{
     db.collection('User').find({UserId : Id, UserPassword : Password}).toArray((err, result)=>{
       if(result.length != 1){
         res.render("login",{pass : true});
@@ -28,19 +27,19 @@ router.get('/login', (req, res, next)=>{
 });
 
 router.get('/create', function(req, res, next) {
-  MongoClient.connect(url,{useNewUrlParser:true}, function(err, client){
-    const db = client.db(dbName);
+  
+  mongoClient.connect(function(err,db,client){
     const query = req.query;
 
-    var userObject = {UserId : query.UserId, UserPassword : query.UserPassword, UserName : query.UserName, Grade : query.Grade, Class : query.Class, Number : query.Number};
+    var userObject = {UserId : query.UserId, UserPassword : query.UserPassword, UserName : query.UserName,
+       Grade : query.Grade, Class : query.Class, Number : query.Number, UserType : query.UserType};
     db.collection('User').insertOne(userObject, (err,recode)=>{
       if(err){
         res.send("값을 입력하는데 실패했습니다. : " + err);
       }
       res.send("값을 성공적으로 입력했습니다.");
       client.close();
-    })
-    
+    });
   });
 });
 
