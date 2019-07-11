13bb2aa3524a24e8a075c22495f8ef1138f9334c
Jang-Ji-Hyun <s18018@gsm.hs.kr>
2019-07-11 11:36:08 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -118,21 +118,6 @@ router.get('/create', function(req, res, next) {
 });
 
 
-// router.connect('login_lent'); // 관리될 DB 서버의 url
-// app.use(bodyParser.urlencoded({extended : true}));
-// app.use(bodyParser.json()); 
-// app.use(session({
-//     secret : "*********", // 이 값을 이용해 암호화옵션
-//     resave : false, // 세션이 수정되지 않아도 항상 저장할지 확인하는 옵션
-//     saveUninitialized : true, // 세션이 uninitalized 상태로 미리 만들어서 저장하는지 묻는 옵션
-//     store: new MongoStore({  // 세션이 서버 메모리가 아닌 어떤 저장소에 들어갈지 정하는 옵션
-//         mongooseConnection: mongoose.connection,
-//         ttl: 60 * 30 // 1시간후 폭파
-//     }),
-//     cookie : { // 쿠키에 들어가는 세션 ID값의 옵션
-//         maxAge : 1000 * 60 * 2 // 2분후 폭파
-//     }
-// }));
 
 
 module.exports = router;
